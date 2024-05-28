2a2609282f48b6a10b6ee7f578c07b86261849c2
Raheem Ullah <raheemullah010101@gmail.com>
2024-05-28 15:46:16 +0500

    Commit Name

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cargo_Shipment.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cargo_Shipment.Startup))]
+namespace Cargo_Shipment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
