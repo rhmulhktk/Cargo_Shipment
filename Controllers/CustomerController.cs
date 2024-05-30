a3eab555ff6583bbf5e8fdcb26b75304e4d35767
Raheem Ullah <raheemullah010101@gmail.com>
2024-05-30 15:03:58 +0500

    Updated All Layouts

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cargo_Shipment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
